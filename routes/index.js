1c6f1fca0ff5daaf79a6f357175527964e7648e5
Tom Fischer <tgfischer6@gmail.com>
2016-10-15 04:33:26 -0400

    Working on index

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var listings = require('../config/nasdaq-listing');
 var Company = require('../models/Company');
 var Sentiment = require("../models/Sentiment");
 var router = express.Router();
-var request = require("request"); 
+var request = require("request");
 var dq = require('datatables-query')
 
 // var API_KEY = "18bcbc1c281f1431245daff8bbc743e7469e05cc";
@@ -11,13 +11,6 @@ var API_KEY = "7814c8d4f65421498296b5c92824b41944f81bdd";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  
-  Company.insertMany(listings, function(err, companies) {
-    res.render('index', {
-      companies: companies
-    });
-  });
-  
   res.render('index');
 });
 
@@ -37,9 +30,9 @@ router.post('/get_listings', function(req, res, next) {
 /* GET sentiment data for 'company' */
 router.get("/news/:_id", function(req, res, next) {
   var companyId = req.params._id;
-  var requestURI = "https://access.alchemyapi.com/calls/data/GetNews?apikey=" + API_KEY + "&return=enriched.url.title,enriched.url.enrichedTitle.docSentiment&rank=high&start=now-40d&end=now&q.enriched.url.enrichedTitle.entities.entity=|text=" + "Google" + ",type=company|&count=2000&dedup=true&outputMode=json"; 
+  var requestURI = "https://access.alchemyapi.com/calls/data/GetNews?apikey=" + API_KEY + "&return=enriched.url.title,enriched.url.enrichedTitle.docSentiment&rank=high&start=now-40d&end=now&q.enriched.url.enrichedTitle.entities.entity=|text=" + "Google" + ",type=company|&count=2000&dedup=true&outputMode=json";
 
-  /*The route will be passed the _id of the company. We can do a find by id here to pick out the correct company for making 
+  /*The route will be passed the _id of the company. We can do a find by id here to pick out the correct company for making
     the request, and then also use the company _id when saving the sentimentArray into the correct company. Use a callback
     after getting the full array of sentiment data to execute the save all and company save. */
   var handleResponse = function(err, response, body, callback) {
@@ -52,19 +45,19 @@ router.get("/news/:_id", function(req, res, next) {
       if (body.status && body.statusInfo) {
         res.send ("We could not get a response: " + body.statusInfo);
       }
-      // We successfully retrieved the sentiment data for this company     
+      // We successfully retrieved the sentiment data for this company
       if (body.result && body.result.docs && body.result.docs.length) {
         for (var i = 0; i < body.result.docs.length; i++) {
           var item = body.result.docs[i];
-          if (item && item.source && item.source.enriched && item.source.enriched.url && item.source.enriched.url.enrichedTitle 
+          if (item && item.source && item.source.enriched && item.source.enriched.url && item.source.enriched.url.enrichedTitle
                   && item.source.enriched.url.enrichedTitle.docSentiment && item.timestamp) {
-            
+
             // Pull out the important information from the current result.
             var docSentiment = item.source.enriched.url.enrichedTitle.docSentiment;
             var score = docSentiment.score;
             var unixTime = new Date(item.timestamp * 1000);
-            var finalDate = new Date(unixTime.getFullYear(), unixTime.getMonth(), unixTime.getDate(), unixTime.getHours(), 
-                  unixTime.getMinutes(), unixTime.getSeconds(), 0);  
+            var finalDate = new Date(unixTime.getFullYear(), unixTime.getMonth(), unixTime.getDate(), unixTime.getHours(),
+                  unixTime.getMinutes(), unixTime.getSeconds(), 0);
 
             var sentimentObject = {
               score: score,
@@ -73,15 +66,15 @@ router.get("/news/:_id", function(req, res, next) {
             arrayOfSentiments.push(sentimentObject);
           }
         }
-          
+
         /* After there has been all the sentiments pushed into the array of sentiments, let's compress this array to each day.
           When running through the array, check what the date is. If it matches a current entry, modify the score of that entry.
-          If it does not match, create a new entry. */  
+          If it does not match, create a new entry. */
         for (var i = 0; i < arrayOfSentiments.length; i++) {
           var exists = false;
           for (var j = 0; j < finalArrayOfSentiments.length; j++) {
             if (finalArrayOfSentiments[j].date.getDay() === arrayOfSentiments[i].date.getDay()
-                  && finalArrayOfSentiments[j].date.getMonth() === arrayOfSentiments[i].date.getMonth() 
+                  && finalArrayOfSentiments[j].date.getMonth() === arrayOfSentiments[i].date.getMonth()
                   && finalArrayOfSentiments[j].date.getFullYear() === arrayOfSentiments[i].date.getFullYear()) {
               finalArrayOfSentiments[j].score += arrayOfSentiments[i].score;
               finalArrayOfSentiments[j].count = finalArrayOfSentiments[j].count + 1;
@@ -106,14 +99,14 @@ router.get("/news/:_id", function(req, res, next) {
           finalArrayOfSentiments[i].score = (finalArrayOfSentiments[i].score / finalArrayOfSentiments[i].count);
           delete finalArrayOfSentiments[i].count;
         }
-        callback(finalArrayOfSentiments); 
+        callback(finalArrayOfSentiments);
       } // end first if
     }
   };
 
   // Fire off the request to the AlchemyNews API to gather more sentiment data
   request({
-    uri: requestURI, 
+    uri: requestURI,
     method: "GET",
     timeout: 10000
   }, function(err, response, body) {
@@ -138,15 +131,15 @@ router.get("/news/:_id", function(req, res, next) {
                   if (insert_err) {
                     res.send("Error updating company sentiment information: " + insert_err);
                   } else {
-                    res.send ("Successfully obtained the sentiment data & stored in company: " + body);     
+                    res.send ("Successfully obtained the sentiment data & stored in company: " + body);
                   }
                 });
               }
             });
           }
-        }); 
+        });
       });
-    }); 
+    });
   });
 
 
